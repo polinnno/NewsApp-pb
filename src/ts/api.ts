b372a4bf82cc3ecb4f6c60cf6466648aa70c3a9a
polinnno <polkorams@gmail.com>
2025-06-09 10:36:18 +0200

    opt

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -2,7 +2,6 @@ import { Category, Article } from './types.js';
 
 
 export const Headlines = 'https://gnews.io/api/v4/top-headlines';
-export const Search = 'https://gnews.io/api/v4/search';
 export const Key = '8d47045c7a0c8f7e23d1aabc81c13255';
 // 84d8e7bb81129ed8635907e3a4b2e053
 // 0f42a5902b659f390f7e86316d8eb0cc
@@ -30,16 +29,4 @@ export async function fetchHeadlinesByCategory(category: Category): Promise<Arti
     const res = await fetch(`${Headlines}?category=${category}&lang=en&apikey=${Key}`);
     const data = await res.json();
     return (data.articles as any[]).map(article => transformToArticle(article, category));
-}
-
-// Used for search or article-specific fetching
-export async function fetchSearchResults(query: string): Promise<Article[]> {
-    const res = await fetch(`${Search}?q=${encodeURIComponent(query)}&lang=en&apikey=${Key}`);
-    const data = await res.json();
-    return data.articles.map(transformToArticle);
-}
-
-export async function fetchFirstMatchingArticle(query: string): Promise<Article | null> {
-    const articles = await fetchSearchResults(query);
-    return articles.length ? articles[0] : null;
 }
